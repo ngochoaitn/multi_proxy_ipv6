c3636722f1c68fdfe91cbf383c81e24a694fc016
FVIA <130783224+fviatool@users.noreply.github.com>
2023-11-13 01:01:24 +0700

    Update rotate_3proxy.sh

--- rotate_3proxy.sh
+++ rotate_3proxy.sh
@@ -1,15 +1,10 @@
-#!/bin/bash
-
-# Function to generate a random string
+#!/bin/sh
 random() {
     tr </dev/urandom -dc A-Za-z0-9 | head -c5
     echo
 }
 
-# Array for generating IPv6 address
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
-
-# Function to generate an IPv6 address with random values
 gen64() {
     ip64() {
         echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
@@ -17,9 +12,8 @@ gen64() {
     echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
 
-# Function to install 3proxy
 install_3proxy() {
-    echo "Installing 3proxy..."
+    echo "installing 3proxy"
     URL="https://raw.githubusercontent.com/ngochoaitn/multi_proxy_ipv6/main/3proxy-3proxy-0.8.6.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
     cd 3proxy-3proxy-0.8.6
@@ -32,7 +26,6 @@ install_3proxy() {
     cd $WORKDIR
 }
 
-# Function to generate 3proxy configuration
 gen_3proxy() {
     cat <<EOF
 daemon
@@ -53,14 +46,12 @@ $(awk -F "/" '{print "auth strong\n" \
 EOF
 }
 
-# Function to generate proxy file for user
 gen_proxy_file_for_user() {
     cat >proxy.txt <<EOF
 $(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
 EOF
 }
 
-# Function to upload proxy configuration
 upload_proxy() {
     local PASS=$(random)
     zip --password $PASS proxy.zip proxy.txt
@@ -71,70 +62,60 @@ upload_proxy() {
     echo "Password: ${PASS}"
 }
 
-# Function to generate data for proxies
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
         echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
-# Function to generate iptables rules
 gen_iptables() {
     cat <<EOF
 $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
 EOF
 }
 
-# Function to generate ifconfig commands
 gen_ifconfig() {
     cat <<EOF
 $(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
 EOF
 }
 
-# Function to rotate proxies and restart 3proxy service
 rotate_proxy() {
     echo "Rotating proxies..."
     service 3proxy restart
 }
 
-# Set up automatic rotation in crontab
-(crontab -l ; echo "*/10 * * * * ${WORKDIR}/rotate_proxy") | crontab -
+# Tự động xoay proxy sau mỗi 10 phút
+(crontab -l ; echo "*/10 * * * * ${WORKDIR}/rotate_proxy.sh") | crontab -
 
-echo "Installing required packages..."
+echo "installing apps"
 yum -y install gcc net-tools bsdtar zip >/dev/null
 
-# Set working folder
+install_3proxy
+
+echo "working folder = /home/proxy-installer"
 WORKDIR="/home/proxy-installer"
+WORKDATA="${WORKDIR}/data.txt"
 mkdir $WORKDIR && cd $_
 
-# Get external IPv4 and IPv6 addresses
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
-echo "Internal IP = ${IP4}. External subnet for IPv6 = ${IP6}"
+echo "Internal ip = ${IP4}. External sub for ip6 = ${IP6}"
 
-COUNT=1000
-echo "Creating $COUNT proxies..."
+echo "How many proxies do you want to create? Example 1000"
+read COUNT
 
 FIRST_PORT=10000
 LAST_PORT=$(($FIRST_PORT + $COUNT))
 
-# Generate data for proxies
 gen_data >$WORKDIR/data.txt
-
-# Generate iptables rules
 gen_iptables >$WORKDIR/boot_iptables.sh
-
-# Generate ifconfig commands
 gen_ifconfig >$WORKDIR/boot_ifconfig.sh
-
 chmod +x ${WORKDIR}/boot_*.sh /etc/rc.local
 
-# Generate 3proxy configuration
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
-# Add commands to rc.local for startup
 cat >>/etc/rc.local <<EOF
 bash ${WORKDIR}/boot_iptables.sh
 bash ${WORKDIR}/boot_ifconfig.sh
@@ -142,11 +123,8 @@ ulimit -n 10048
 service 3proxy start
 EOF
 
-# Run rc.local
 bash /etc/rc.local
 
-# Generate proxy file for user
 gen_proxy_file_for_user
 
-# Upload proxy configuration
 upload_proxy
