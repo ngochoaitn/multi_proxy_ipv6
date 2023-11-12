164e1e56cb52c85d571b3b0ed56267387de4155f
FVIA <130783224+fviatool@users.noreply.github.com>
2023-11-13 04:34:25 +0700

    Update xoay.sh

--- xoay.sh
+++ xoay.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+# Function to generate a random string
 random() {
     tr </dev/urandom -dc A-Za-z0-9 | head -c5
     echo
 }
 
+# Array for hexadecimal values
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
 
+# Function to generate an IPv6 address
 gen64() {
     ip64() {
         echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
@@ -14,6 +17,7 @@ gen64() {
     echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
 
+# Function to install 3proxy
 install_3proxy() {
     echo "Installing 3proxy..."
     URL="https://raw.githubusercontent.com/ngochoaitn/multi_proxy_ipv6/main/3proxy-3proxy-0.8.6.tar.gz"
@@ -21,22 +25,16 @@ install_3proxy() {
     cd 3proxy-3proxy-0.8.6
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-
-    # Stop 3proxy if it's running
-    systemctl stop 3proxy
-
-    # Replace 3proxy binary forcefully
     yes | cp -f src/3proxy /usr/local/etc/3proxy/bin/
-
-    # Start 3proxy again
+    systemctl stop 3proxy
     systemctl start 3proxy
-
     cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
     chmod +x /etc/init.d/3proxy
     chkconfig 3proxy on
     cd $WORKDIR
 }
 
+# Function to configure IP whitelist for 3proxy
 auth_ip_config() {
     echo "auth iponly"
     while read -r allowed_ip; do
@@ -44,6 +42,7 @@ auth_ip_config() {
     done < allowed_ips.txt
 }
 
+# Function to generate 3proxy configuration
 gen_3proxy() {
     auth_ip_config
     cat <<EOF
@@ -55,32 +54,29 @@ setgid 65535
 setuid 65535
 flush
 auth strong
-
 users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
-
-$(awk -F "/" '{print "auth strong\n" \
-"allow " $1 "\n" \
-"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
-"flush\n"}' ${WORKDATA})
+$(awk -F "/" '{print "auth strong\nallow " $1 "\nproxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\nflush\n"}' ${WORKDATA})
 EOF
 }
 
+# Function to upload proxy details
 upload_proxy() {
     local PASS=$(random)
     echo "$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})" > proxy.txt
     URL=$(curl -s --upload-file proxy.txt https://transfer.sh/proxy.txt)
-
     echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
     echo "Download proxy list from: ${URL}"
     echo "Password: ${PASS}"
 }
 
+# Function to generate data for proxy
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
         echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
+# Function to generate iptables rules
 gen_iptables() {
     cat <<EOF
 iptables -A INPUT -p tcp --dport 3128 -m state --state NEW -j ACCEPT
@@ -90,162 +86,59 @@ $(awk -F "/" '{print "iptables -A INPUT -p tcp --dport " $4 " -s " $3 " -m state
 EOF
 }
 
+# Function to generate ifconfig commands
 gen_ifconfig() {
     cat <<EOF
 $(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
 EOF
 }
 
+# Function to rotate proxies
 rotate_proxy() {
     echo "Rotating proxies..."
     service 3proxy restart
 }
 
-# Tự động xoay proxy sau mỗi 10 phút
+# Cron job for automatic proxy rotation every 10 minutes
 (crontab -l ; echo "*/10 * * * * ${WORKDIR}/rotate_3proxy.sh") | crontab -
 
-echo "installing apps"
+# Installing required packages
+echo "Installing necessary packages..."
 yum -y install gcc net-tools bsdtar zip >/dev/null
 
+# Installing and configuring 3proxy
 install_3proxy
 
-echo "working folder = /home/proxy-installer"
+# Setting up working folder
+echo "Setting up working folder..."
 WORKDIR="/home/proxy-installer"
 WORKDATA="${WORKDIR}/data.txt"
 mkdir -p $WORKDIR && cd $_
 
+# Obtaining IP addresses
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. External sub for ip6 = ${IP6}"
 
-echo "Bạn muốn tạo bao nhiêu proxy? Ví dụ 2000"
+# Asking user for the number of proxies to create
+echo "How many proxies do you want to create? Example: 2000"
 read COUNT
 
 FIRST_PORT=10000
 LAST_PORT=$(($FIRST_PORT + $COUNT))
 
+# Generating data, iptables rules, ifconfig commands, 3proxy configuration
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
 gen_ifconfig >$WORKDIR/boot_ifconfig.sh
 chmod +x ${WORKDIR}/boot_*.sh /etc/rc.local
-
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
-configuration for Squid
-acl to_ipv6 dst ipv6
-access_log none
-cache deny all
-dns_nameservers 1.1.1.1
-max_filedesc 65535
-coredump_dir /var/spool/squid
-acl QUERY urlpath_regex cgi-bin \?
-refresh_pattern ^ftp:           1440    20%     10080
-refresh_pattern ^gopher:        1440    0%      1440
-refresh_pattern -i (/cgi-bin/|\?) 0     0%      0
-refresh_pattern .               0       20%     4320
 
-# Authentication parameters for Squid
-auth_param basic program /usr/local/squid/libexec/basic_ncsa_auth /etc/squid/squid.passwords
-auth_param basic children 1024
-auth_param basic realm Proxy
-auth_param basic credentialsttl 2 hours
-auth_param basic casesensitive off
-acl password proxy_auth REQUIRED
+# Configuring Squid
+# (You can add your Squid configuration here)
 
-# Include Squid port configuration
-include /etc/squid/ports.conf
-
-# HTTP access control
-http_access deny !to_ipv6
-http_access allow password
-http_access allow to_ipv6
-http_access allow all
-cache deny all
-
-# Request header access configuration
-request_header_access follow_x_forwarded_for deny all
-request_header_access X-Forwarded-For deny all
-request_header_access Allow allow all
-request_header_access Authorization allow all
-request_header_access WWW-Authenticate allow all
-request_header_access Proxy-Authorization allow all
-request_header_access Proxy-Authenticate allow all
-request_header_access Cache-Control allow all
-request_header_access Transfer-Encoding allow all
-request_header_access Content-Encoding allow all
-request_header_access Content-Length allow all
-request_header_access Content-Type allow all
-request_header_access Date allow all
-request_header_access Expires allow all
-request_header_access Host allow all
-request_header_access If-Modified-Since allow all
-request_header_access Last-Modified allow all
-request_header_access Location allow all
-request_header_access Pragma allow all
-request_header_access Accept allow all
-request_header_access Accept-Charset allow all
-request_header_access Accept-Encoding allow all
-request_header_access Accept-Language allow all
-request_header_access Content-Language allow all
-request_header_access Mime-Version allow all
-request_header_access Retry-After allow all
-request_header_access Title allow all
-request_header_access Content-Encoding allow all
-request_header_access Content-Length allow all
-request_header_access Content-Type allow all
-request_header_access Date allow all
-request_header_access Expires allow all
-request_header_access Host allow all
-request_header_access If-Modified-Since allow all
-request_header_access Last-Modified allow all
-request_header_access Location allow all
-request_header_access Pragma allow all
-request_header_access Accept allow all
-request_header_access Accept-Charset allow all
-request_header_access Accept-Encoding allow all
-request_header_access Accept-Language allow all
-request_header_access Content-Language allow all
-request_header_access Mime-Version allow all
-request_header_access Retry-After allow all
-request_header_access Title allow all
-request_header_access Connection allow all
-request_header_access Proxy-Connection allow all
-request_header_access User-Agent allow all
-request_header_access Referer allow all
-request_header_access Cookie allow all
-request_header_access Set-Cookie allow all
-request_header_access Content-Disposition allow all
-request_header_access Range allow all
-request_header_access Accept-Ranges allow all
-request_header_access Vary allow all
-request_header_access Etag allow all
-request_header_access If-None-Match allow all
-request_header_replace Referer example.com
-request_header_access All deny all
-request_header_access From deny all
-request_header_access Referer deny all
-request_header_access User-Agent deny all
-reply_header_access Via deny all
-reply_header_access Server deny all
-reply_header_access WWW-Authenticate deny all
-reply_header_access Link deny all
-reply_header_access Allow allow all
-reply_header_access Proxy-Authenticate allow all
-reply_header_access Cache-Control allow all
-reply_header_access Content-Encoding allow all
-reply_header_access Content-Length allow all
-reply_header_access Content-Type allow all
-reply_header_access Date allow all
-reply_header_access Expires allow all
-reply_header_access Last-Modified allow all
-reply_header_access Location allow all
-reply_header_access Pragma allow all
-reply_header_access Content-Language allow all
-reply_header_access Retry-After allow all
-reply_header_access Title allow all
-reply_header_access Content-Disposition allow all
-reply_header_access Connection allow all
-reply_header_access All deny all
+# Adding commands to rc.local for startup
 cat >>/etc/rc.local <<EOF
 bash ${WORKDIR}/boot_iptables.sh
 bash ${WORKDIR}/boot_ifconfig.sh
@@ -253,10 +146,16 @@ ulimit -n 10048
 service 3proxy start
 EOF
 
+# Starting services
 bash /etc/rc.local
 
+# Generating proxy files for users
 gen_proxy_file_for_user
 
+# Configuring IP whitelist for 3proxy
 auth_ip_config
 
+# Uploading proxy details
 upload_proxy
+
+# End of script
