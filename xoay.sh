abfb03cc8595a2443ac210f71b385d45d251d3a5
FVIA <130783224+fviatool@users.noreply.github.com>
2023-11-13 03:59:38 +0700

    Update xoay.sh

--- xoay.sh
+++ xoay.sh
@@ -1,10 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+curl -sO https://raw.githubusercontent.com/quanglinh0208/3proxy/main/ipv6.sh && bash ipv6.sh
+
 random() {
     tr </dev/urandom -dc A-Za-z0-9 | head -c5
     echo
 }
 
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
+
 gen64() {
     ip64() {
         echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
@@ -13,7 +17,7 @@ gen64() {
 }
 
 install_3proxy() {
-    echo "installing 3proxy"
+    echo "Installing 3proxy..."
     URL="https://raw.githubusercontent.com/ngochoaitn/multi_proxy_ipv6/main/3proxy-3proxy-0.8.6.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
     cd 3proxy-3proxy-0.8.6
@@ -37,7 +41,12 @@ setuid 65535
 flush
 auth strong
 
-users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
+$(awk -F "/" '{print "auth strong\n" \
+"allow " $1 "\n" \
+"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
+"flush\n"}' ${WORKDATA})
+EOF
+}
 
 auth_ip_config() {
     echo "auth iponly"
@@ -58,7 +67,7 @@ upload_proxy() {
 
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
+        echo "usr$(random)/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
@@ -82,15 +91,22 @@ rotate_proxy() {
     service 3proxy restart
 }
 
-# Tự động xoay proxy sau mỗi 10 phút
-(crontab -l ; echo "*/10 * * * * ${WORKDIR}/rotate_3proxy.sh") | crontab -
+auto_rotate() {
+    (crontab -l ; echo "*/10 * * * * ${WORKDIR}/rotate_3proxy.sh") | crontab -
+    echo "Added cron job to run the script every 10 minutes."
+}
 
-echo "installing apps"
+auto_restart() {
+    (crontab -l ; echo "0 0 * * * ${WORKDIR}/restart_if_empty.sh") | crontab -
+    echo "Added cron job to restart if data is empty."
+}
+
+echo "Installing apps..."
 yum -y install gcc net-tools bsdtar zip >/dev/null
 
 install_3proxy
 
-echo "working folder = /home/proxy-installer"
+echo "Working folder = /home/proxy-installer"
 WORKDIR="/home/proxy-installer"
 WORKDATA="${WORKDIR}/data.txt"
 mkdir $WORKDIR && cd $_
@@ -98,9 +114,9 @@ mkdir $WORKDIR && cd $_
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
-echo "Internal ip = ${IP4}. External sub for ip6 = ${IP6}"
+echo "Internal IP = ${IP4}. External sub for IP6 = ${IP6}"
 
-echo "How many proxies do you want to create? Example 500"
+echo "How many proxies do you want to create? Example: 2000"
 read COUNT
 
 FIRST_PORT=10000
@@ -122,8 +138,12 @@ EOF
 
 bash /etc/rc.local
 
-gen_proxy_file_for_user
-
 auth_ip_config
 
 upload_proxy
+
+auto_rotate
+
+auto_restart
+
+echo "Script execution completed successfully!"
