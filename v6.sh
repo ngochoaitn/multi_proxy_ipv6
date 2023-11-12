fc2d95c52f43bf3c6440a7d9254b5e9789999b3f
FVIA <130783224+fviatool@users.noreply.github.com>
2023-11-13 06:17:43 +0700

    Update v6.sh

--- v6.sh
+++ v6.sh
@@ -1,10 +1,15 @@
-#!/bin/sh
+#!/bin/bash
+
+# Function to generate a random string
 random() {
     tr </dev/urandom -dc A-Za-z0-9 | head -c5
     echo
 }
 
+# Array for hexadecimal values
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
+
+# Function to generate an IPv6 address
 gen64() {
     ip64() {
         echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
@@ -12,21 +17,40 @@ gen64() {
     echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
 
+# Function to install 3proxy
 install_3proxy() {
-    echo "Đang cài đặt 3proxy..."
+    echo "Installing 3proxy..."
     URL="https://raw.githubusercontent.com/ngochoaitn/multi_proxy_ipv6/main/3proxy-3proxy-0.8.6.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
     cd 3proxy-3proxy-0.8.6
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-    cp src/3proxy /usr/local/etc/3proxy/bin/
+    yes | cp -f src/3proxy /usr/local/etc/3proxy/bin/
+    systemctl stop 3proxy
+    systemctl start 3proxy
     cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
     chmod +x /etc/init.d/3proxy
     chkconfig 3proxy on
     cd $WORKDIR
 }
 
+# Function to configure IP whitelist for 3proxy
+auth_ip_config() {
+    echo "auth iponly"
+    while read -r allowed_ip; do
+        echo "allow $allowed_ip"
+        
+        # Add to IPv4
+        iptables -I INPUT -p tcp -s "$allowed_ip" -j ACCEPT
+        
+        # Add to IPv6
+        ip6tables -I INPUT -p tcp -s "$allowed_ip" -j ACCEPT
+    done < "${WORKDIR}/allowed_ips.txt"
+}
+
+# Function to generate 3proxy configuration
 gen_3proxy() {
+    auth_ip_config
     cat <<EOF
 daemon
 maxconn 1000
@@ -36,109 +60,89 @@ setgid 65535
 setuid 65535
 flush
 auth strong
-
 users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
-
-auth_ip_config
-
-$(awk -F "/" '{print "auth strong\n" \
-"allow " $1 "\n" \
-"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
-"flush\n"}' ${WORKDATA})
+$(awk -F "/" '{print "auth strong\nallow " $1 "\nproxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\nflush\n"}' ${WORKDATA})
 EOF
 }
 
-auth_ip_config() {
-    echo "auth iponly"
-    while read -r allowed_ip; do
-        echo "allow $allowed_ip"
-    done < allowed_ips.txt
-}
-
+# Function to upload proxy details
 upload_proxy() {
     local PASS=$(random)
     echo "$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})" > proxy.txt
     URL=$(curl -s --upload-file proxy.txt https://transfer.sh/proxy.txt)
-
-    echo "Proxy đã sẵn sàng! Định dạng IP:PORT:TÀI KHOẢN:MẬT KHẨU"
-    echo "Tải danh sách proxy từ: ${URL}"
-    echo "Mật khẩu: ${PASS}"
+    echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
+    echo "Download proxy list from: ${URL}"
+    echo "Password: ${PASS}"
 }
 
+# Function to generate data for proxy
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
         echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
+# Function to generate iptables rules
 gen_iptables() {
     cat <<EOF
-iptables -A INPUT -p tcp --dport 3128 -m state --state NEW -j ACCEPT
-iptables -A INPUT -p tcp --dport 1080 -m state --state NEW -j ACCEPT
-iptables -A INPUT -p tcp --dport 8080 -m state --state NEW -j ACCEPT
-$(awk -F "/" '{print "iptables -A INPUT -p tcp --dport " $4 " -s " $3 " -m state --state NEW -j ACCEPT"}' ${WORKDATA})
+systemctl mask firewalld
+systemctl enable iptables
+systemctl stop firewalld
+yum install iptables-services -y
+systemctl enable iptables
+systemctl start iptables
+systemctl enable ip6tables
+systemctl start ip6tables
+echo "Thiết lập tường lửa...."
 EOF
 }
 
-gen_ifconfig() {
-    cat <<EOF
-$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
-EOF
-}
-
+# Function to rotate proxies
 rotate_proxy() {
-    echo "Xoay proxy..."
+    echo "Rotating proxies..."
     service 3proxy restart
 }
 
-reset_on_empty_data() {
-    if [ ! -s ${WORKDATA} ]; then
-        echo "Không tìm thấy dữ liệu. Đang đặt lại..."
-        reset_all
-    fi
-}
-
-reset_all() {
-    echo "Đang đặt lại..."
-    service 3proxy stop
-    rm -rf ${WORKDATA} proxy.txt
-    main
-}
-
-# Tự động xoay proxy sau mỗi 10 phút
+# Cron job for automatic proxy rotation every 10 minutes
 (crontab -l ; echo "*/10 * * * * ${WORKDIR}/rotate_3proxy.sh") | crontab -
 
-# Tự động đặt lại nếu hết dữ liệu
-(crontab -l ; echo "*/30 * * * * ${WORKDIR}/reset_on_empty_data.sh") | crontab -
-
-echo "Đang cài đặt các ứng dụng cần thiết"
+# Installing required packages
+echo "Installing necessary packages..."
 yum -y install gcc net-tools bsdtar zip >/dev/null
 
+# Installing and configuring 3proxy
 install_3proxy
 
-echo "Thư mục làm việc = /home/proxy-installer"
+# Setting up working folder
+echo "Setting up working folder..."
 WORKDIR="/home/proxy-installer"
 WORKDATA="${WORKDIR}/data.txt"
-mkdir $WORKDIR && cd $_
+mkdir -p $WORKDIR && cd $_
 
+# Obtaining IP addresses
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
-echo "IP nội bộ = ${IP4}. Dải IP6 bên ngoài = ${IP6}"
+echo "Internal ip = ${IP4}. External sub for ip6 = ${IP6}"
 
-echo "Bạn muốn tạo bao nhiêu proxy? Ví dụ 2000"
+# Asking user for the number of proxies to create
+echo "How many proxies do you want to create? Example: 2000"
 read COUNT
 
 FIRST_PORT=10000
 LAST_PORT=$(($FIRST_PORT + $COUNT))
 
+# Generating data, iptables rules, ifconfig commands, 3proxy configuration
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
 gen_ifconfig >$WORKDIR/boot_ifconfig.sh
 chmod +x ${WORKDIR}/boot_*.sh /etc/rc.local
-
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
+# Configuring Squid
+# (You can add your Squid configuration here)
+
+# Adding commands to rc.local for startup
 cat >>/etc/rc.local <<EOF
 bash ${WORKDIR}/boot_iptables.sh
 bash ${WORKDIR}/boot_ifconfig.sh
@@ -146,10 +150,11 @@ ulimit -n 10048
 service 3proxy start
 EOF
 
+# Starting services
 bash /etc/rc.local
 
-gen_proxy_file_for_user
-
+# Configuring IP whitelist for 3proxy
 auth_ip_config
 
+# Uploading proxy details
 upload_proxy
