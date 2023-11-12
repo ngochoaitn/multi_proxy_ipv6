74e5cc72de4d4d3ee87a10e87c6631b797e0904a
FVIA <130783224+fviatool@users.noreply.github.com>
2023-11-13 03:43:02 +0700

    Update xoay.sh

--- xoay.sh
+++ xoay.sh
@@ -80,7 +80,7 @@ EOF
 
 gen_ifconfig() {
     cat <<EOF
-$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
+$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/16"}' ${WORKDATA})
 EOF
 }
 
