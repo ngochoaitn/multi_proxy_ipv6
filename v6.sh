6143f5fd72253a43c74b951535c5d2c13f158c54
FVIA <130783224+fviatool@users.noreply.github.com>
2023-11-13 03:23:46 +0700

    Update v6.sh

--- v6.sh
+++ v6.sh
@@ -1,4 +1,132 @@
-êu proxy? Ví dụ 2000"
+#!/bin/sh
+random() {
+    tr </dev/urandom -dc A-Za-z0-9 | head -c5
+    echo
+}
+
+array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
+gen64() {
+    ip64() {
+        echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+    }
+    echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
+}
+
+install_3proxy() {
+    echo "Đang cài đặt 3proxy..."
+    URL="https://raw.githubusercontent.com/ngochoaitn/multi_proxy_ipv6/main/3proxy-3proxy-0.8.6.tar.gz"
+    wget -qO- $URL | bsdtar -xvf-
+    cd 3proxy-3proxy-0.8.6
+    make -f Makefile.Linux
+    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+    cp src/3proxy /usr/local/etc/3proxy/bin/
+    cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
+    chmod +x /etc/init.d/3proxy
+    chkconfig 3proxy on
+    cd $WORKDIR
+}
+
+gen_3proxy() {
+    cat <<EOF
+daemon
+maxconn 1000
+nscache 65536
+timeouts 1 5 30 60 180 1800 15 60
+setgid 65535
+setuid 65535
+flush
+auth strong
+
+users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
+
+auth_ip_config
+
+$(awk -F "/" '{print "auth strong\n" \
+"allow " $1 "\n" \
+"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
+"flush\n"}' ${WORKDATA})
+EOF
+}
+
+auth_ip_config() {
+    echo "auth iponly"
+    while read -r allowed_ip; do
+        echo "allow $allowed_ip"
+    done < allowed_ips.txt
+}
+
+upload_proxy() {
+    local PASS=$(random)
+    echo "$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})" > proxy.txt
+    URL=$(curl -s --upload-file proxy.txt https://transfer.sh/proxy.txt)
+
+    echo "Proxy đã sẵn sàng! Định dạng IP:PORT:TÀI KHOẢN:MẬT KHẨU"
+    echo "Tải danh sách proxy từ: ${URL}"
+    echo "Mật khẩu: ${PASS}"
+}
+
+gen_data() {
+    seq $FIRST_PORT $LAST_PORT | while read port; do
+        echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
+    done
+}
+
+gen_iptables() {
+    cat <<EOF
+iptables -A INPUT -p tcp --dport 3128 -m state --state NEW -j ACCEPT
+iptables -A INPUT -p tcp --dport 1080 -m state --state NEW -j ACCEPT
+iptables -A INPUT -p tcp --dport 8080 -m state --state NEW -j ACCEPT
+$(awk -F "/" '{print "iptables -A INPUT -p tcp --dport " $4 " -s " $3 " -m state --state NEW -j ACCEPT"}' ${WORKDATA})
+EOF
+}
+
+gen_ifconfig() {
+    cat <<EOF
+$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
+EOF
+}
+
+rotate_proxy() {
+    echo "Xoay proxy..."
+    service 3proxy restart
+}
+
+reset_on_empty_data() {
+    if [ ! -s ${WORKDATA} ]; then
+        echo "Không tìm thấy dữ liệu. Đang đặt lại..."
+        reset_all
+    fi
+}
+
+reset_all() {
+    echo "Đang đặt lại..."
+    service 3proxy stop
+    rm -rf ${WORKDATA} proxy.txt
+    main
+}
+
+# Tự động xoay proxy sau mỗi 10 phút
+(crontab -l ; echo "*/10 * * * * ${WORKDIR}/rotate_3proxy.sh") | crontab -
+
+# Tự động đặt lại nếu hết dữ liệu
+(crontab -l ; echo "*/30 * * * * ${WORKDIR}/reset_on_empty_data.sh") | crontab -
+
+echo "Đang cài đặt các ứng dụng cần thiết"
+yum -y install gcc net-tools bsdtar zip >/dev/null
+
+install_3proxy
+
+echo "Thư mục làm việc = /home/proxy-installer"
+WORKDIR="/home/proxy-installer"
+WORKDATA="${WORKDIR}/data.txt"
+mkdir $WORKDIR && cd $_
+
+IP4=$(curl -4 -s icanhazip.com)
+IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
+
+echo "IP nội bộ = ${IP4}. Dải IP6 bên ngoài = ${IP6}"
+
+echo "Bạn muốn tạo bao nhiêu proxy? Ví dụ 2000"
 read COUNT
 
 FIRST_PORT=10000
