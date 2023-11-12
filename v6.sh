2283a9648f1c037f1c866f0c5098d6c1863f485f
FVIA <130783224+fviatool@users.noreply.github.com>
2023-11-13 06:21:10 +0700

    Update v6.sh

--- v6.sh
+++ v6.sh
@@ -1,160 +1,126 @@
-#!/bin/bash
+#!/bin/sh
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
-# Function to generate a random string
 random() {
-    tr </dev/urandom -dc A-Za-z0-9 | head -c5
-    echo
+	tr </dev/urandom -dc A-Za-z0-9 | head -c5
+	echo
 }
 
-# Array for hexadecimal values
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
-
-# Function to generate an IPv6 address
 gen64() {
-    ip64() {
-        echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-    }
-    echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
+	ip64() {
+		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+	}
+	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
-
-# Function to install 3proxy
 install_3proxy() {
     echo "Installing 3proxy..."
-    URL="https://raw.githubusercontent.com/ngochoaitn/multi_proxy_ipv6/main/3proxy-3proxy-0.8.6.tar.gz"
+    URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
     wget -qO- $URL | bsdtar -xvf-
     cd 3proxy-3proxy-0.8.6
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-    yes | cp -f src/3proxy /usr/local/etc/3proxy/bin/
-    systemctl stop 3proxy
-    systemctl start 3proxy
-    cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
-    chmod +x /etc/init.d/3proxy
-    chkconfig 3proxy on
+    cp src/3proxy /usr/local/etc/3proxy/bin/
     cd $WORKDIR
 }
-
-# Function to configure IP whitelist for 3proxy
-auth_ip_config() {
-    echo "auth iponly"
-    while read -r allowed_ip; do
-        echo "allow $allowed_ip"
-        
-        # Add to IPv4
-        iptables -I INPUT -p tcp -s "$allowed_ip" -j ACCEPT
-        
-        # Add to IPv6
-        ip6tables -I INPUT -p tcp -s "$allowed_ip" -j ACCEPT
-    done < "${WORKDIR}/allowed_ips.txt"
+download_proxy() {
+cd /home/cloudfly
+curl -F "file=@proxy.txt" https://file.io
 }
-
-# Function to generate 3proxy configuration
 gen_3proxy() {
-    auth_ip_config
     cat <<EOF
 daemon
-maxconn 1000
+maxconn 2000
+nserver 1.1.1.1
+nserver 8.8.4.4
+nserver 2001:4860:4860::8888
+nserver 2001:4860:4860::8844
 nscache 65536
 timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
+stacksize 6291456 
 flush
 auth strong
+
 users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
-$(awk -F "/" '{print "auth strong\nallow " $1 "\nproxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\nflush\n"}' ${WORKDATA})
+
+$(awk -F "/" '{print "auth strong\n" \
+"allow " $1 "\n" \
+"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
+"flush\n"}' ${WORKDATA})
 EOF
 }
 
-# Function to upload proxy details
-upload_proxy() {
-    local PASS=$(random)
-    echo "$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})" > proxy.txt
-    URL=$(curl -s --upload-file proxy.txt https://transfer.sh/proxy.txt)
-    echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
-    echo "Download proxy list from: ${URL}"
-    echo "Password: ${PASS}"
+
+gen_proxy_file_for_user() {
+    cat >proxy.txt <<EOF
+$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
+EOF
 }
 
-# Function to generate data for proxy
+
 gen_data() {
+    userproxy=$(random)
+    passproxy=$(random)
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
+        echo "$userproxy/$passproxy/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
-# Function to generate iptables rules
-gen_iptables() {
+gen_ifconfig() {
     cat <<EOF
-systemctl mask firewalld
-systemctl enable iptables
-systemctl stop firewalld
-yum install iptables-services -y
-systemctl enable iptables
-systemctl start iptables
-systemctl enable ip6tables
-systemctl start ip6tables
-echo "Thiết lập tường lửa...."
+$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
 EOF
 }
+echo "installing apps"
+yum -y install wget gcc net-tools bsdtar zip >/dev/null
 
-# Function to rotate proxies
-rotate_proxy() {
-    echo "Rotating proxies..."
-    service 3proxy restart
-}
+cat << EOF > /etc/rc.d/rc.local
+#!/bin/bash
+touch /var/lock/subsys/local
+EOF
 
-# Cron job for automatic proxy rotation every 10 minutes
-(crontab -l ; echo "*/10 * * * * ${WORKDIR}/rotate_3proxy.sh") | crontab -
+echo "installing apps"
+yum -y install wget gcc net-tools bsdtar zip >/dev/null
 
-# Installing required packages
-echo "Installing necessary packages..."
-yum -y install gcc net-tools bsdtar zip >/dev/null
-
-# Installing and configuring 3proxy
 install_3proxy
 
-# Setting up working folder
-echo "Setting up working folder..."
-WORKDIR="/home/proxy-installer"
+echo "working folder = /home/cloudfly"
+WORKDIR="/home/cloudfly"
 WORKDATA="${WORKDIR}/data.txt"
-mkdir -p $WORKDIR && cd $_
+mkdir $WORKDIR && cd $_
 
-# Obtaining IP addresses
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
-echo "Internal ip = ${IP4}. External sub for ip6 = ${IP6}"
+echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
 
-# Asking user for the number of proxies to create
-echo "How many proxies do you want to create? Example: 2000"
-read COUNT
+while :; do
+  read -p "Enter FIRST_PORT between 10000 and 60000: " FIRST_PORT
+  [[ $FIRST_PORT =~ ^[0-9]+$ ]] || { echo "Enter a valid number"; continue; }
+  if ((FIRST_PORT >= 10000 && FIRST_PORT <= 60000)); then
+    echo "OK! Valid number"
+    break
+  else
+    echo "Number out of range, try again"
+  fi
+done
+LAST_PORT=$(($FIRST_PORT + 750))
+echo "LAST_PORT is $LAST_PORT. Continue..."
 
-FIRST_PORT=10000
-LAST_PORT=$(($FIRST_PORT + $COUNT))
-
-# Generating data, iptables rules, ifconfig commands, 3proxy configuration
 gen_data >$WORKDIR/data.txt
-gen_iptables >$WORKDIR/boot_iptables.sh
-gen_ifconfig >$WORKDIR/boot_ifconfig.sh
-chmod +x ${WORKDIR}/boot_*.sh /etc/rc.local
+chmod +x boot_*.sh /etc/rc.local
+
 gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
-# Configuring Squid
-# (You can add your Squid configuration here)
-
-# Adding commands to rc.local for startup
 cat >>/etc/rc.local <<EOF
-bash ${WORKDIR}/boot_iptables.sh
-bash ${WORKDIR}/boot_ifconfig.sh
-ulimit -n 10048
-service 3proxy start
+/usr/local/etc/3proxy/bin/3proxy /usr/local/etc/3proxy/3proxy.cfg
 EOF
-
-# Starting services
+chmod 0755 /etc/rc.local
 bash /etc/rc.local
 
-# Configuring IP whitelist for 3proxy
-auth_ip_config
+gen_proxy_file_for_user
 
-# Uploading proxy details
-upload_proxy
+echo "Starting Proxy"
+download_proxy
