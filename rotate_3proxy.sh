e97a7e4f5256114fc89e74557befa162ad332e76
FVIA <130783224+fviatool@users.noreply.github.com>
2023-11-14 16:16:40 +0700

    Update rotate_3proxy.sh

--- rotate_3proxy.sh
+++ rotate_3proxy.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Function to generate a random string
 random() {
     tr </dev/urandom -dc A-Za-z0-9 | head -c5
     echo
 }
 
+# Array for hexadecimal values
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
 
+# Function to generate an IPv6 address
 gen64() {
     ip64() {
         echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
@@ -14,6 +17,7 @@ gen64() {
     echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
 
+# Function to install 3proxy
 install_3proxy() {
     echo "Installing 3proxy..."
     URL="https://raw.githubusercontent.com/ngochoaitn/multi_proxy_ipv6/main/3proxy-3proxy-0.8.6.tar.gz"
@@ -37,11 +41,11 @@ timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
 flush
-auth strong
+auth iponly
 
 users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
 
-$(awk -F "/" '{print "auth strong\n" \
+$(awk -F "/" '{print "auth iponly\n" \
 "allow " $1 "\n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
 "flush\n"}' ${WORKDATA})
@@ -64,93 +68,71 @@ upload_proxy() {
     echo "Password: ${PASS}"
 }
 
+# Function to generate data for proxy
 gen_data() {
-    while read -r allowed_ip; do
-        seq $FIRST_PORT $LAST_PORT | while read port; do
-            echo "usr$(random)/$allowed_ip/$port/$(gen64 $IP6)"
-        done
-    done < allowed_ips.txt
+    seq $FIRST_PORT $LAST_PORT | while read port; do
+        echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
+    done
 }
 
+# Function to generate iptables rules
 gen_iptables() {
     cat <<EOF
-$(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
+iptables -A INPUT -p tcp --dport 3128 -m state --state NEW -j ACCEPT
+iptables -A INPUT -p tcp --dport 1080 -m state --state NEW -j ACCEPT
+iptables -A INPUT -p tcp --dport 8080 -m state --state NEW -j ACCEPT
+$(awk -F "/" '{print "iptables -A INPUT -p tcp --dport " $4 " -s " $3 " -m state --state NEW -j ACCEPT"}' ${WORKDATA})
 EOF
 }
 
-gen_ifconfig() {
-    cat <<EOF
-$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
-EOF
-}
-
+# Function to rotate proxies
 rotate_proxy() {
     echo "Rotating proxies..."
     service 3proxy restart
 }
 
-schedule_rotate() {
-    (crontab -l ; echo "*/10 * * * * ${WORKDIR}/rotate_proxy") | crontab -
-    echo "Added cron job to run the script every 10 minutes."
-}
+# Cron job for automatic proxy rotation every 10 minutes
+(crontab -l ; echo "*/10 * * * * ${WORKDIR}/rotate_3proxy.sh") | crontab -
 
-setup_iptables() {
-    # Set up iptables rules
-    systemctl mask firewalld
-    systemctl enable iptables
-    systemctl stop firewalld
-    yum install iptables-services -y
-    systemctl enable iptables
-    systemctl start iptables
-    systemctl enable ip6tables
-    systemctl start ip6tables
+# Installing required packages
+echo "Installing necessary packages..."
+yum -y install gcc net-tools bsdtar zip >/dev/null
 
-    echo "Configuring iptables rules..."
-}
+# Installing and configuring 3proxy
+install_3proxy
 
-main() {
-    setup_iptables
-    install_3proxy
+# Setting up working folder
+echo "Setting up working folder..."
+WORKDIR="/home/proxy-installer"
+WORKDATA="${WORKDIR}/data.txt"
+mkdir -p $WORKDIR && cd $_
 
-    echo "Working folder = /home/proxy-installer"
-    WORKDIR="/home/proxy-installer"
-    WORKDATA="${WORKDIR}/data.txt"
-    mkdir $WORKDIR && cd $_
+# Obtaining IP addresses
+IP4=$(curl -4 -s icanhazip.com)
+IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
-    IP4=$(curl -4 -s icanhazip.com)
-    IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
+echo "Internal ip = ${IP4}. External sub for ip6 = ${IP6}"
 
-    echo "Internal IP = ${IP4}. External sub for IP6 = ${IP6}"
+echo "How many proxies do you want to create? Example: 500"
+read COUNT
 
-    echo "How many proxies do you want to create? Example: 500"
-    read COUNT
+FIRST_PORT=10000
+LAST_PORT=$(($FIRST_PORT + $COUNT))
 
-    FIRST_PORT=10000
-    LAST_PORT=$(($FIRST_PORT + $COUNT))
+# Generating data, iptables rules, 3proxy configuration
+gen_data >$WORKDIR/data.txt
+gen_iptables >$WORKDIR/boot_iptables.sh
+chmod +x ${WORKDIR}/boot_*.sh /etc/rc.local
+gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
 
-    gen_data >$WORKDIR/data.txt
-    gen_iptables >$WORKDIR/boot_iptables.sh
-    gen_ifconfig >$WORKDIR/boot_ifconfig.sh
-    chmod +x ${WORKDIR}/boot_*.sh /etc/rc.local
-
-    gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
-
-    cat >>/etc/rc.local <<EOF
+cat >>/etc/rc.local <<EOF
 bash ${WORKDIR}/boot_iptables.sh
-bash ${WORKDIR}/boot_ifconfig.sh
 ulimit -n 10048
 service 3proxy start
 EOF
 
-    bash /etc/rc.local
+# Starting services
+bash /etc/rc.local
 
-    gen_proxy_file_for_user
-    upload_proxy
-
-    ngen_iptables
-    schedule_rotate
-
-    echo "Script execution completed successfully!"
-}
-
-main
+# Uploading proxy details
+upload_proxy
