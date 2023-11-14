7c9ef75244ff0f871f73b678c0056c132e6ef5e8
FVIA <130783224+fviatool@users.noreply.github.com>
2023-11-14 09:34:54 +0700

    Update ip.sh

--- ip.sh
+++ ip.sh
@@ -44,7 +44,7 @@ timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
 flush
-auth none  # Turn off IP authentication
+auth iponly
 users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
 $(awk -F "/" '{print "auth none\nproxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\nflush\n"}' ${WORKDATA})
 EOF
