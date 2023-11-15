b2ebe6b48233651f084eacf4529e35877a8b7630
FVIA <130783224+fviatool@users.noreply.github.com>
2023-11-15 18:14:19 +0700

    Update v6.sh

--- v6.sh
+++ v6.sh
@@ -1,102 +1,134 @@
-#!/usr/bin/bash
+#!/bin/sh
 
-gen_ipv6_64() {
-    rm ipv6.txt
-    count_ipv6=1
-    while [ "$count_ipv6" -le $MAXCOUNT ]
-    do
-        array=( 1 2 3 4 5 6 7 8 9 0 a b c d e f )
-        ip64() {
-            echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-        }
-        echo $IP6:$(ip64):$(ip64):$(ip64):$(ip64) >> ipv6.txt
-        let "count_ipv6 += 1"
-    done
+random() {
+    tr </dev/urandom -dc A-Za-z0-9 | head -c5
+    echo
+}
+
+array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
+gen64() {
+    ip64() {
+        echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+    }
+    echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
 
 install_3proxy() {
-    echo "Installing 3proxy..."
-    sudo yum install gcc make nano git -y
-    git clone https://github.com/z3apa3a/3proxy
-    cd 3proxy
-    ln -s Makefile.Linux Makefile
-    make
-    sudo make install
-    systemctl daemon-reload
-    echo "* hard nofile 999999" >>  /etc/security/limits.conf
-    echo "* soft nofile 999999" >>  /etc/security/limits.conf
-    systemctl stop firewalld
-    systemctl disable firewalld
-    ulimit -n 65535
+    echo "installing 3proxy"
+    URL="https://raw.githubusercontent.com/ngochoaitn/multi_proxy_ipv6/main/3proxy-3proxy-0.8.6.tar.gz"
+    wget -qO- $URL | bsdtar -xvf-
+    cd 3proxy-3proxy-0.8.6
+    make -f Makefile.Linux
+    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+    cp src/3proxy /usr/local/etc/3proxy/bin/
+    cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
+    chmod +x /etc/init.d/3proxy
     chkconfig 3proxy on
-    cd ~
+    cd $WORKDIR
 }
 
-gen_3proxy_cfg() {
-    echo daemon
-    echo maxconn 3000
-    echo nserver 1.1.1.1
-    echo nserver [2606:4700:4700::1111]
-    echo nserver [2606:4700:4700::1001]
-    echo nserver [2001:4860:4860::8888]
-    echo nscache 65536
-    echo timeouts 1 5 30 60 180 1800 15 60
-    echo setgid 65535
-    echo setuid 65535
-    echo stacksize 6291456
-    echo flush
-    echo auth none
+gen_3proxy() {
+    cat <<EOF
+daemon
+maxconn 1000
+nscache 65536
+timeouts 1 5 30 60 180 1800 15 60
+setgid 65535
+setuid 65535
+flush
+auth strong
 
-    port=$START_PORT
-    while read ip; do
-        echo "proxy -6 -n -a -p$port -i$IP4 -e$ip"
-        ((port+=1))
-    done < ipv6.txt
+users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
+
+$(awk -F "/" '{print "auth strong\n" \
+"allow " $1 "\n" \
+"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
+"flush\n"}' ${WORKDATA})
+EOF
 }
 
-gen_ifconfig() {
-    while read line; do
-        echo "ifconfig $IFCFG inet6 add $line/64"
-    done < ipv6.txt
+gen_proxy_file_for_user() {
+    cat >proxy.txt <<EOF
+$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
+EOF
 }
 
-export_txt(){
-    port=$START_PORT
-    for ((i=1; i<=$MAXCOUNT; i++)); do
-        echo "$IP4:$port"
-        ((port+=1))
+upload_proxy() {
+    local PASS=$(random)
+    zip --password $PASS proxy.zip proxy.txt
+    URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
+
+    echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
+    echo "Download zip archive from: ${URL}"
+    echo "Password: ${PASS}"
+}
+
+gen_data() {
+    seq $FIRST_PORT $LAST_PORT | while read port; do
+        echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
-rotate_proxy() {
-    service 3proxy restart
-    echo "Rotated proxies at $(date)"
+gen_iptables() {
+    cat <<EOF
+$(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
+EOF
 }
 
-auto_rotate() {
-    (crontab -l ; echo "*/10 * * * * ~/rotate_3proxy.sh") | crontab -
-    echo "Added cron job to rotate proxies every 10 minutes."
+gen_ifconfig() {
+    cat <<EOF
+$(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
+EOF
 }
 
-echo "Installing apps..."
-yum -y install gcc net-tools bsdtar zip psmisc >/dev/null
+rotate_proxy_script() {
+    cat <<EOF
+#!/bin/sh
+service 3proxy restart
+EOF
+}
+
+# Tự động xoay proxy sau mỗi 10 phút
+(crontab -l ; echo "*/10 * * * * ${WORKDIR}/rotate_3proxy.sh") | crontab -
+
+echo "installing apps"
+yum -y install gcc net-tools bsdtar zip >/dev/null
 
 install_3proxy
 
-IP4=$(curl ifconfig.me)
+echo "working folder = /home/proxy-installer"
+WORKDIR="/home/proxy-installer"
+WORKDATA="${WORKDIR}/data.txt"
+mkdir $WORKDIR && cd $_
+
+IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
-IFCFG=$(ip route get 2001:4860:4860::8888 | awk -- '{printf $5}')
-START_PORT=14000
-MAXCOUNT={MAXCOUNT}
 
-gen_ipv6_64
-gen_3proxy_cfg > /etc/3proxy/3proxy.cfg
+echo "Internal IP = ${IP4}. External subnet for IP6 = ${IP6}"
 
-cat >> /etc/rc.local <<EOF
-bash ~/boot_ifconfig.sh
+echo "How many proxies do you want to create? Example 500"
+read COUNT
+
+FIRST_PORT=10000
+LAST_PORT=$(($FIRST_PORT + $COUNT))
+
+gen_data >$WORKDIR/data.txt
+gen_iptables >$WORKDIR/boot_iptables.sh
+gen_ifconfig >$WORKDIR/boot_ifconfig.sh
+rotate_proxy_script >$WORKDIR/rotate_3proxy.sh
+chmod +x ${WORKDIR}/boot_*.sh ${WORKDIR}/rotate_3proxy.sh /etc/rc.local
+
+gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
+
+cat >>/etc/rc.local <<EOF
+bash ${WORKDIR}/boot_iptables.sh
+bash ${WORKDIR}/boot_ifconfig.sh
+ulimit -n 10048
 service 3proxy start
 EOF
 
-auto_rotate
+bash /etc/rc.local
 
-echo "Script execution completed successfully!"
+gen_proxy_file_for_user
+
+upload_proxy
