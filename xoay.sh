12b2c044057648960bf72e5a8ae5c1a7a11a568a
FVIA <130783224+fviatool@users.noreply.github.com>
2023-11-15 17:33:29 +0700

    Update xoay.sh

--- xoay.sh
+++ xoay.sh
@@ -55,13 +55,12 @@ EOF
 
 upload_proxy() {
     local PASS=$(random)
-    zip --password $PASS proxy.zip proxy.txt
-    URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.txt)
+    echo "$(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})" > proxy.txt
+    URL=$(curl -s --upload-file proxy.txt https://transfer.sh/proxy.txt)
 
     echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
-    echo "Download zip archive from: ${URL}"
+    echo "Download proxy list from: ${URL}"
     echo "Password: ${PASS}"
-
 }
 
 gen_data() {
