d104dbccf04cfb419afdf6de6bf486f3d645bbcc
FVIA <130783224+fviatool@users.noreply.github.com>
2023-11-15 18:22:01 +0700

    Update v6.sh

--- v6.sh
+++ v6.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+# Function to generate a random string
 random() {
     tr </dev/urandom -dc A-Za-z0-9 | head -c5
     echo
 }
 
+# Array for generating IPv6
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
+
+# Function to generate an IPv6 address
 gen64() {
     ip64() {
         echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
@@ -13,6 +17,7 @@ gen64() {
     echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
 
+# Function to install 3proxy
 install_3proxy() {
     echo "installing 3proxy"
     URL="https://raw.githubusercontent.com/ngochoaitn/multi_proxy_ipv6/main/3proxy-3proxy-0.8.6.tar.gz"
@@ -27,8 +32,10 @@ install_3proxy() {
     cd $WORKDIR
 }
 
+# Function to generate 3proxy configuration
 gen_3proxy() {
     cat <<EOF
+# 3proxy configuration
 daemon
 maxconn 1000
 nscache 65536
@@ -47,12 +54,14 @@ $(awk -F "/" '{print "auth strong\n" \
 EOF
 }
 
+# Function to generate proxy file for users
 gen_proxy_file_for_user() {
     cat >proxy.txt <<EOF
 $(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
 EOF
 }
 
+# Function to upload proxy details
 upload_proxy() {
     local PASS=$(random)
     zip --password $PASS proxy.zip proxy.txt
@@ -63,24 +72,28 @@ upload_proxy() {
     echo "Password: ${PASS}"
 }
 
+# Function to generate data for proxies
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
         echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
+# Function to generate iptables rules
 gen_iptables() {
     cat <<EOF
 $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
 EOF
 }
 
+# Function to generate ifconfig commands
 gen_ifconfig() {
     cat <<EOF
 $(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
 EOF
 }
 
+# Function to generate script for rotating proxies
 rotate_proxy_script() {
     cat <<EOF
 #!/bin/sh
@@ -88,7 +101,7 @@ service 3proxy restart
 EOF
 }
 
-# Tự động xoay proxy sau mỗi 10 phút
+# Automatic rotation of proxies every 10 minutes
 (crontab -l ; echo "*/10 * * * * ${WORKDIR}/rotate_3proxy.sh") | crontab -
 
 echo "installing apps"
@@ -104,9 +117,9 @@ mkdir $WORKDIR && cd $_
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
-echo "Internal IP = ${IP4}. External subnet for IP6 = ${IP6}"
+echo "Internal ip = ${IP4}. External sub for ip6 = ${IP6}"
 
-echo "How many proxies do you want to create? Example 500"
+echo "How many proxy do you want to create? Example 500"
 read COUNT
 
 FIRST_PORT=10000
