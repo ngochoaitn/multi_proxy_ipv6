120154ac1d1e98075e58c8cacafeabafa53ed926
FVIA <130783224+fviatool@users.noreply.github.com>
2023-11-15 18:58:18 +0700

    Update xoay.sh

--- xoay.sh
+++ xoay.sh
@@ -106,10 +106,8 @@ if [ -d "$WORKDIR" ]; then
 fi
 
 # Create the directory
-mkdir "$WORKDIR" && cd "$WORKDIR"
+mkdir "$WORKDIR" && cd "$WORKDIR" || exit 1
 WORKDATA="${WORKDIR}/data.txt"
-mkdir $WORKDIR && cd $WORKDIR || exit 1
-
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
