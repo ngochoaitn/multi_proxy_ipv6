b141af6c479b767b6a680bde77a2191af79f2267
FVIA <130783224+fviatool@users.noreply.github.com>
2023-11-15 19:00:06 +0700

    Update xoay.sh

--- xoay.sh
+++ xoay.sh
@@ -6,6 +6,7 @@ random() {
 }
 
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
+
 gen64() {
     ip64() {
         echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
